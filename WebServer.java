74db4e05ae96d5610aca82b545934712042a84be
Isabel Lee <ijl0322@gmail.com>
2017-01-23 15:25:38 -0600

    add opening file

--- WebServer.java
+++ WebServer.java
@@ -2,7 +2,7 @@ import java.io.* ;
 import java.net.* ;
 import java.util.* ;
 
-public final class WebServer
+public class WebServer
 {
 	public static void main(String argv[]) throws Exception
 	{
@@ -35,29 +35,50 @@ class RequestHandle implements Runnable
 	public void run()
 	{
 		try {
-			processRequest();
+			getRequest();
 		} catch(Exception e) {
 			System.out.println("There is an Error: " + e);
 		}
 	}
 
-	private void processRequest() throws Exception
+	private void getRequest() throws Exception
 	{
 
 		PrintWriter outStream = new PrintWriter(socket.getOutputStream());
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-		String requestLine = reader.readLine();
+		//Find out the filename requested and the type of command(HEAD, GET...)
+		String requestLine = bufferedReader.readLine();
+		String root = "www";
+		String fileName = requestLine.split(" ")[1];
+		String requestCommand = requestLine.split(" ")[0];
+		System.out.println("Get File : " + fileName);
+		System.out.println("Request Command: " + requestCommand);
 
+		//Printing out the request
 		System.out.println();
 		System.out.println(requestLine);
-
-		String header = "";
-		while ((header = reader.readLine()).length() != 0) {
-			System.out.println(header);
+		String request_contents = "";
+		while ((request_contents = bufferedReader.readLine()).length() != 0) {
+			System.out.println(request_contents);
 		}
 
+		//Opening File
+		FileInputStream fileInputStream = null;
+		boolean fileExists = true;
+
+		try {
+			fileInputStream = new FileInputStream(root + fileName);
+		} catch (FileNotFoundException e) {
+			fileExists = false;
+		}
+
+		if (fileExists) {
+			System.out.println("File Exists.");
+		} else {
+			System.out.println("File Does Not Exist.");
+		}
 
 		outStream.println("HTTP/1.1 200 OK");
 		outStream.println("MIME_version:1.0");
@@ -66,8 +87,9 @@ class RequestHandle implements Runnable
 		outStream.println("Content_Length:" + content.length());
 		outStream.println("");
 		outStream.println(content);
+
 		outStream.close();
-		reader.close();
+		bufferedReader.close();
 		socket.close();
 	}
 
